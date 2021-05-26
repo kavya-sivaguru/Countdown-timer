970203f7d6911da47652c10fc37cdeaca241ada6
Kavya .S.R <80980373+kavya-sivaguru@users.noreply.github.com>
2021-05-26 10:12:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 var display=document.createElement('div');
 display.setAttribute('class', 'show')
 document.body.append(display);
-var down = 11;
-let countdown = () => {
-    down = down - 1;
-    if(down <= 0){
-       down = "Happy Independence Day";
-   }   
-    display.innerText =down;
-}
-    setTimeout(countdown, 1000);
-    setTimeout(countdown, 2000);
-    setTimeout(countdown, 3000);
-    setTimeout(countdown, 4000);
-    setTimeout(countdown, 5000);
-    setTimeout(countdown, 6000);
-    setTimeout(countdown, 7000);
-    setTimeout(countdown, 8000);
-    setTimeout(countdown, 9000);
-    setTimeout(countdown, 10000);
-    setTimeout(countdown, 11000);
+
+
+    setTimeout(() =>{   
+    display.innerText =10;
+    setTimeout(() =>{
+        display.innerText =9;
+        setTimeout(() =>{
+            display.innerText =8;
+            setTimeout(() =>{
+                display.innerText =7;
+                setTimeout(() =>{
+                    display.innerText =6;
+                    setTimeout(() =>{
+                        display.innerText =5;
+                        setTimeout(() =>{
+                            display.innerText =4;
+                            setTimeout(() =>{
+                                display.innerText =3;
+                                setTimeout(() =>{
+                                    display.innerText =2;
+                                    setTimeout(() =>{
+                                        display.innerText =1;
+                                        setTimeout(() =>{
+                                            display.innerText ="Happy Independence day";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+   
+    
     
    
 
